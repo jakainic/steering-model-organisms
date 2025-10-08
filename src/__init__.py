8b878f054b39831d62a010b44f6f7e35c06c2f30
Jenna Kainic <jkainic@gmail.com>
2025-10-08 16:10:48 +0000

    permissions

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
-# Package init
+"""Package initialization for src.
+
+Loads environment variables from a `.env` file if present.
+"""
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv(), override=False)
